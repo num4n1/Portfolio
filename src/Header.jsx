9868b02409ab448a59d8fd7d34e7491b3ee84448
Mohamed Numan <business.numan@gmail.com>
2024-11-12 22:23:47 -0500

    improved profile page and navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,45 +1,26 @@
+import Logo from './assets/MN-logo-no-bg-gold.png'
+import React, { useState } from "react";
 import "./stylesheets/Header.css";
-import Logo from './assets/MN-logo-no-bg.png'
 
 function Header() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleNavbar = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="custom-navbar">
       <img className="nav-bar-logo" src={Logo}></img>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
+      <ul className={`navbar-links ${isDropdownVisible ? "active" : ""}`}>
+        <li><a href="#profile">Profile</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#companies">Companies</a></li>
+        <li><a href="#feedback">Feedback</a></li>
+      </ul>
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        ☰
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
-              Profile
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Projects
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Companies
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Feedback
-            </a>
-          </li>
-        </ul>
-      </div>
     </nav>
   );
 }
